eacd3599c52adde32c93e2e76fef4b7c769fa601
Felipe Cantu <36748934+ABACABB00@users.noreply.github.com>
2020-03-28 20:34:20 -0500

    javascript firebase working, alertbox working favicon working and node modes added

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,18 +1,17 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyC-57Lpi7xBvb7cxUUiTO2Iwa-Ft25HjoQ",
-    authDomain: "port-form.firebaseapp.com",
-    databaseURL: "https://port-form.firebaseio.com",
-    projectId: "port-form",
-    storageBucket: "port-form.appspot.com",
-    messagingSenderId: "1002529863476",
-    appId: "1:1002529863476:web:682291f6ba6a841a9e9761",
-    measurementId: "G-JVW4WWDRVG"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+var fconfig = {
+  apiKey: "AIzaSyDrUaHf9hr1GQEEmi6WKWgR6B197XQUZ_M",
+  authDomain: "form-application-640ec.firebaseapp.com",
+  databaseURL: "https://form-application-640ec.firebaseio.com",
+  projectId: "form-application-640ec",
+  storageBucket: "form-application-640ec.appspot.com",
+  messagingSenderId: "486904912418",
+  appId: "1:486904912418:web:73fdf990a918087e9a087b",
+  measurementId: "G-94RDJ8WL3J"
+};
 
-  var messagesRef = firebase.database().rel('messages');
+firebase.initializeApp(fconfig);
+
+var usersRef = firebase.database().ref('users');
 
   document.getElementById('contactMe').addEventListener('submit', submitForm);
 
@@ -21,9 +20,10 @@ var firebaseConfig = {
 
       var name = getInputVal('name');
       var email = getInputVal('email');
+      var phone = getInputVal('phone');
       var message = getInputVal('message');
 
-      saveMessage(name, email, message);
+      saveUser(name, email, phone, message);
 
       document.querySelector('.alert').getElementsByClassName.display = 'block';
 
@@ -34,15 +34,31 @@ var firebaseConfig = {
       document.getElementById('contactMe').reset();
   }
 
+
   function getInputVal(id) {
-    return document.getElementById(id).Value;
+    return document.getElementById(id).value;
   }
 
-  function saveMessage(email, name, message) {
-      var newMessageRef = messagesRef.push();
-      newMessageRef.set({
+  function saveUser(email, name, phone, message) {
+      var newUsersRef = usersRef.push();
+      newUsersRef.set({
           email: email,
           name: name,
-          massage: massage
+          phone: phone,
+          message: message
       });
   }
+
+
+
+  // function validation() {
+  //   email = document.getElementById('email').value;
+  //   name = document.getElementById('name').value;
+
+  //   if((email == email) && (name == name)) {
+  //     swal("Good job!", "Form Submitted", "Thank You!")
+  //   } 
+  //   else {
+  //     sweetAlert("Oops...", "Something went wrong!", "error");
+  //   }
+  // }
